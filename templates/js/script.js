1da636ee200a8107759b423fd449c5f05034246a
Nishchay Patel <86916051+nishchay1576@users.noreply.github.com>
2022-06-25 21:57:50 -0400

    Add files via upload

---
+++ templates/js/script.js
@@ -0,0 +1,34 @@
+
+const spans =document.querySelectorAll('h1 span')
+spans.forEach(span => span.addEventListener('mouseover',function(e) {
+    span.classList.add('animated','rubberBand')
+}))
+spans.forEach(span => span.addEventListener('mouseout',function(e) {
+    span.classList.remove('animated','rubberBand')
+}))
+
+
+
+const showRequiredCategory = event => {
+    const getId =event.id
+    const links = document.querySelectorAll('.work-category button')
+    for(i=0; i<links.length; i++)
+    {
+        if(links[i].hasAttribute('class')){
+            links[i].classList.remove('active')
+        }
+    }
+    event.classList.add('active')
+    const getCategory = document.querySelector(`.category-${getId}`)
+    const categories = document.querySelectorAll('div[class ^= "category-"]')
+    for(i=0;i<categories.length;i++)
+    {
+        if(categories[i].hasAttribute('class')){
+            categories[i].classList.remove('showCategory')
+            categories[i].classList.add('hideCategory')
+        }
+    }
+    getCategory.classList.remove('hideCategory');
+    getCategory.classList.add('showCategory')
+}
+
